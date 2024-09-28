ce5c1c705eb27409a99ea3f867e71620ed0a9b2a
Yuki Yamamoto <yuki120908yg@icloud.com>
2024-09-28 10:33:08 +0900

    Create animation while fetching the job data

--- app/jobs/page.tsx
+++ app/jobs/page.tsx
@@ -6,14 +6,18 @@ import useSWR from "swr";
 // データ取得のための関数
 const fetcher = (url: string) => fetch(url).then((res) => res.json());
 
-
-// サーバーサイドでデータを取得
 export default function JobListPage() {
   // useSWRでデータをキャッシュして取得
   const { data: jobs, error } = useSWR("/api/jobs", fetcher);
 
   if (error) return <div>エラーが発生しました: {error.message}</div>;
-  if (!jobs) return <div>読み込み中</div>;
+  if (!jobs) {
+    return (
+      <div className="flex items-center justify-center min-h-screen">
+        <div className="w-16 h-16 border-4 border-blue-500 border-t-transparent border-solid rounded-full animate-spin">a</div>
+      </div>
+    );
+  }
 
   return (
     <div>
